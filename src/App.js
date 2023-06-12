92fc40a1670d7829fcf9cf34ebe0c55135d3d709
shadybonne <shadybonne@gmail.com>
2023-06-12 09:27:24 +0100

    alx

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Event from './Event';
+
+const App = () => {
+  return (
+    <div>
+      <Event/>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/Event.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    navbar.classList.toggle('active');
+    menu.classList.toggle('fa-times');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    menu.classList.remove('fa-times');
+}
+
+function Event() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+
+export default Event;
